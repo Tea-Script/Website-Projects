283a9cbd59653561d7d689b4144ba46ad3efc49e
Tea-Script <mciara@bu.edu>
2017-04-02 17:44:26 -0400

    debugging

--- StatusUpdate/index.php
+++ StatusUpdate/index.php
@@ -29,16 +29,18 @@
     <script src="./app.js"></script>
     <?php echo "<script type='text/javascript'>alert('This page is under construction');</script>";
           if($_POST){
-            if(isset($_POST["post"])){
+            echo "post request received";
+            /*if(isset($_POST["post"])){
               post($_POST["post"]);
               break;
 
-            }
+            }*/
 
 
           }
           elseif($_GET){
             //exit;
+            echo "get request received";
 
           }
 
@@ -49,39 +51,6 @@
                   </script>" ;
             //exit;
           }
-
-/*
-    switch($_SERVER['REQUEST_METHOD'])
-    {
-      case 'GET':
-        if(file_exists('chat.txt') && filesize('chat.txt') > 0){
-          $filesize = filesize('chat.txt');
-          $f = fopen('chat.txt', "a+");
-          $line = fread($f, $filesize);
-          $line = htmlspecialchars(str_replace("\n", "", $line));
-          echo "<script> post = '" . $line  . "';</script>";
-          fclose($f);
-          fclose(fopen('chat.txt', 'w'));
-
-        }
-
-
-
-        break;
-
-      case 'POST':
-        $function = $_POST['function'];
-        $log = Array();
-        switch ($function) {
-          case('send'):
-            $message = strip_tags($_POST['message']);
-            $f = fopen('chat.txt', 'w');
-            fwrite($f, $message);
-            fclose($f);
-            break;
-        }
-    }*/
-
     ?>
   </body>
 </html>
