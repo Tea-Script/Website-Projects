fdd626ac60ae22dceefadf6dc12ef92344eaefe9
Tea-Script <mciara@bu.edu>
2017-04-02 22:15:50 -0400

    debugging update

--- StatusUpdate/chat.php
+++ StatusUpdate/chat.php
@@ -14,7 +14,7 @@ switch($_SERVER['REQUEST_METHOD'])
       else{
           echo ""; //app.js only posts if it receives a non null input to callback function
       }
-      //$messages = array();
+      unset($messages);
       break;
 
   case 'POST':
