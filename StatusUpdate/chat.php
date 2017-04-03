410256b978d858cd2c6af9e376cd0aca80c17b6f
Tea-Script <mciara@bu.edu>
2017-04-02 22:12:49 -0400

    debugging update

--- StatusUpdate/chat.php
+++ StatusUpdate/chat.php
@@ -1,20 +1,20 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
-      
+
 if(!isset($messages)){
   $messages = array();
 }
 switch($_SERVER['REQUEST_METHOD'])
 {
   case 'GET':
-      if(! isempty($messages)){
+      if(! empty($messages)){
           echo json_encode($messages); //submit array of messages to post
       }
       else{
           echo ""; //app.js only posts if it receives a non null input to callback function
       }
-      //$messages = array();
+      $messages = array();
       break;
 
   case 'POST':
