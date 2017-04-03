385c80b46fc8fcf3f9952503587c61e6b308c05e
Tea-Script <mciara@bu.edu>
2017-04-02 23:34:02 -0400

    minor update

--- StatusUpdate/chat.php
+++ StatusUpdate/chat.php
@@ -1,22 +1,22 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
-
+$home = "/home/ubuntu/";
 switch($_SERVER['REQUEST_METHOD'])
 {
   case 'GET':
-      if(file_exists($_SERVER['HOME'] . "/chat.txt")){
-          echo file_get_contents($_SERVER['HOME'] . "/chat.txt"); //submit csv messages to post
+      if(file_exists($home . "chat.txt")){
+          echo file_get_contents($home . "chat.txt"); //submit csv messages to post
       }
       else{
           echo ""; //app.js only posts if it receives a non null input to callback function
       }
-      file_put_contents($_SERVER['HOME'] . "/chat.txt", '');
+      file_put_contents($home . "chat.txt", '');
       break;
 
   case 'POST':
       $msg = $_POST['send'];
-      $f = fopen($_SERVER['HOME'] . "/chat.txt", 'a');
+      $f = fopen($home . "chat.txt", 'a');
       fwrite($f, $msg . ",");
       fclose($f);
       echo "Message: $msg posted";
