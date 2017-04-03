644541181d4e9f1b10bc2a5d413360b19b55909d
Tea-Script <mciara@bu.edu>
2017-04-02 21:40:32 -0400

    debugging update

--- StatusUpdate/chat.php
+++ StatusUpdate/chat.php
@@ -1,24 +1,21 @@
 <?php
+$messages = array();
 switch($_SERVER['REQUEST_METHOD'])
 {
   case 'GET':
-      if(isset($messages)){
+      if(!isempty($messages)){
           echo json_encode($messages); //submit array of messages to post
       }
       else{
           echo ""; //app.js only posts if it receives a non null input to callback function
       }
-      unset($messages);
+      global $messages = array();
       break;
 
   case 'POST':
       $msg = $_POST['send'];
-      if(isset($messages)){
-          $messages[] = $msg;
-      }
-      else{
-        static $messages = array($msg);
-      }
+      $messages[] = $msg;
+
       echo "Message: $msg posted";
       break;
 }
