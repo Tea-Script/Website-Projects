ddeb047d692077bf1194cd6d666561efd19985a0
Tea-Script <mciara@bu.edu>
2017-04-14 15:02:01 -0400

    secret folder

---
+++ HiddenFolder/GoodSecurity/LawAmsNameGame/matching.js
@@ -0,0 +1,44 @@
+function main(){
+
+    images = ["alex.jpg", "audrey.jpg", "erin.jpg", "eugene.jpg", "ivanna.jpg", "jeanluc.jpg", "maria.jpg", "tamm.jpg", "vivien.jpg", "andrew.jpg",
+              "dan.jpg", "maia.jpg", "nicole.jpg", "jackie.jpg", "haydn.jpg", "matt.jpg", "sam.jpg", "arthur.jpg", "chris.jpg", "niv.jpg", "falco.jpg","david.jpg"
+
+            ];
+    var img = document.getElementById("current");
+    img.src = "./" + images[0]
+    img.name = images[0].substring(0, images[0].indexOf('.'));
+    var inpt = document.getElementById("answer");
+    inpt.addEventListener("keydown", function(e){
+        var key = e.which || e.keyCode || 0;
+        var txt = this.value;
+        console.log(typeof txt);
+        txt = txt.toLowerCase();
+        txt = txt === "ar0d" ? "alex" : txt;
+        txt = txt === "matthew" ? "matt": txt;
+        txt = txt === "samuel" ? "sam" : txt;
+        txt = txt === "namhee" ? "nicole" : txt;
+        txt = txt === "artman" ? "arthur" : txt;
+        txt = txt === "brawn" ? "arthur" : txt;
+        txt = txt.replace(/[^a-zA-Z]/g, '');
+
+        console.log(txt);
+        if(key === 13){
+            this.value = "";
+            if(txt === img.name){
+              alert("correct!");
+              var nxt = Math.floor(Math.random() * images.length);
+              var nxtImg = "./"+ images[nxt];
+              while(nxtImg === img.src){
+                nxt = Math.floor(Math.random() * images.length);
+                nxtImg = "./"+ images[nxt];
+
+              }
+              img.src = nxtImg
+              img.name = images[nxt].substring(0, images[nxt].indexOf('.'));
+            }
+
+        }
+
+
+    });
+}
