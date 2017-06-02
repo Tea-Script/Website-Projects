7d2f32ac80ebe975d79f9f990ade1c85111421b5
root <root@ip-172-31-36-16.ec2.internal>
2017-06-02 15:14:30 +0000

    for testing SQL

---
+++ SQLTEST/Chat/chat.php
@@ -0,0 +1,38 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+switch($_SERVER['REQUEST_METHOD'])
+{
+  case 'GET':
+
+      if(file_exists("chat.txt")){
+          //$posts = count(preg_split("," , file_get_contents("chat.txt"))) - 1;
+          echo file_get_contents("chat.txt"); //submit csv messages to post
+          //if($posts >= 7){
+          //}
+      }
+      else{
+          echo ""; //app.js only posts if it receives a non null input to callback function
+      }
+      break;
+
+  case 'POST':
+      if(isset($_POST['send'])){
+        $msg = $_POST['send'];
+        $f = fopen("chat.txt", 'a');
+        fwrite($f, "\n" . $msg );
+        fclose($f);
+        echo $msg;
+      }
+      elseif(isset($_POST['req'])){
+        $posts = count(file("chat.txt"));
+        if($posts > 50 || $posts <= 0){
+          file_put_contents('chat.txt', "");
+          $posts = 0;
+        }
+        echo $posts;
+      }
+     break;
+}
+
+?>
